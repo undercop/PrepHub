1263b1f0c95abcf9b0d50a40259228fbda99e1f1
karthik_raj <raj.karthik5566@gmail.com>
2025-04-04 20:44:31 +0530

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+const Authlayout = ({children}: {children: ReactNode}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Authlayout

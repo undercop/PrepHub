98fb5334193271208473e758b09a4feb3487bbfe
karthik_raj <raj.karthik5566@gmail.com>
2025-04-05 19:43:23 +0530

    ui of auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,17 +1,60 @@
 "use client"
 
-import {z} from "zod "
-
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import Image from "next/image"
 const formSchema = z.object({
     username: z.string().min(2).max(50),
 })
 
 
 const AuthForm = () => {
+   // 1. Define your form.
+   const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
   return (
-    <div>
+    <div className="card-border lg:min-w-[566px]">
+      <div className=" flex flex-col gap-6 card py-14 px-10">
+ <div className="flex flex-row gap-2 justify-center">
+   <Image src="/logo.svg" alt="logo" height={32} width={32}/>
+   <h2 className="text-primary-100 ">PrepHub</h2>
+
+ </div>
+ <h3> practice job interview with AI</h3>
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)
+
+    } className="w-full space-y-6 mt-4 ">
+      <p>Name</p>
       
-    </div>
+      <Button type="submit">Submit</Button>
+    </form>
+  </Form>
+        </div>
+  </div>
   )
 }
 
---
+++ components/FormField.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FormControl, FormDescription , FormItem , FormLabel , FormMessage } from './ui/form'
+import { Input } from './ui/input'
+const FormField = () => {
+  return (
+    <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input placeholder="shadcn" {...field} />
+            </FormControl>
+            <FormDescription>
+              This is your public display name.
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+        />
+  )
+}
+
+export default FormField
